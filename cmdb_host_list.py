7cd83250573f0c833020447dd838d508a49f4838
limix <coollimix@gmail.com>
2022-07-12 19:48:45 +0800

    添加web-auth安全认证逻辑

--- cmdb_host_list.py
+++ cmdb_host_list.py
@@ -1,7 +1,9 @@
 from __future__ import (absolute_import, division, print_function)
 
+import base64
 import json
 import logging
+from urllib import request, parse
 
 from ansible.errors import AnsibleParserError
 from ansible.module_utils.common.text.converters import to_text, to_native
@@ -22,6 +24,10 @@ DOCUMENTATION = r'''
             type: str
         password:
             description: 密码
+            required: false
+            type: str
+        url:
+            description: 地址
             required: false
             type: str
 '''
@@ -60,22 +66,54 @@ class InventoryModule(BaseInventoryPlugin):
         # TODO 添加插件以及特性
         try:
             # HTTP请求获取需要的结果 TODO 添加安全认证逻辑,添加相关参数,定义主机分组的格式信息
-            # head = {"Content-Type":"application/json; charset=UTF-8", 'Connection': 'close'}
-            # url_d = "http://10.222.222.222:30037/ability/syncOrderInfo"
-            # res_d = requests.post(url=url_d,data=json_d,headers=head)
-            logging.debug(str.format("username:{}, password:{}", self.get_option("username"), self.get_options("password")))
-            res_body = '{"group1":{"hosts":[{"ip":"192.168.3.101","port":22,"ansible_ssh_pass":"admin"},{"ip":"192.168.3.102","port":22,"ansible_ssh_pass":"admin"}]}}'
-            host_dict = json.loads(res_body)
-            for group in host_dict:
+            # 发送请求
+            # url = "http://192.168.3.36:8899/inventories/1"
+            config = self._read_config_data(path)
+
+            url = self.get_option("url")
+            username = self.get_option("username")
+            password = self.get_option("password")
+            auth="Basic "+base64.b64encode(("%s:%s" % (username, password)).encode()).decode()
+            req = request.Request(url, headers={"Authorization":auth}, method="GET")
+            response = request.urlopen(req)
+            text = response.read()
+            host_dict = json.loads(text.decode('utf-8'))
+            #logging.debug(str.format("username:{}, password:{}", self.get_option("username"), self.get_options("password")))
+            #res_body = '{"group1":{"hosts":[{"ip":"192.168.3.101","port":22,"ansible_ssh_pass":"admin"},{"ip":"192.168.3.102","port":22,"ansible_ssh_pass":"admin"}]}}'
+            for group in host_dict["groups"]:
                 # 初始化group
-                self.inventory.add_group(group)
-                for host in host_dict[group]['hosts']:
+                self.inventory.add_group(group['name'])
+                for host in group['hosts']:
                     ansible_host = host.pop('ip')
                     ansible_port = host.pop('port')
-                    self.inventory.add_host(ansible_host, group=group, port=ansible_port)
+                    self.inventory.add_host(ansible_host, group=group['name'], port=ansible_port)
                     # 给host添加其他参数,如: ansible_ssh_pass
                     for k, v in host.items():
-                        self.inventory.set_variable(ansible_host, k, v)
+                        if k.startswith("ansible_"):
+                            self.inventory.set_variable(ansible_host, k, v)
         except Exception as e:
             # INFO 如果发生异常需要抛出解析异常
-            raise AnsibleParserError('Invalid data from string, could not parse: %s' % to_native(e))
+            raise AnsibleParserError('Invalid data from string, could not parse: %s' % to_native(repr(e)))
+
+
+if __name__ == '__main__':
+    try:
+        url = "http://192.168.3.124:8899/inventories/awx"
+        username = "awx"
+        password = "awx"
+        auth="Basic "+base64.b64encode(("%s:%s" % (username, password)).encode()).decode()
+        req = request.Request(url, headers={"Authorization":auth}, method="GET")
+        response = request.urlopen(req)
+        text = response.read()
+        host_dict = json.loads(text.decode('utf-8'))
+        for group in host_dict["groups"]:
+            # 初始化group
+            print(group['name'])
+            for host in group['hosts']:
+                ansible_host = host.pop('ip')
+                ansible_port = host.pop('port')
+                for k, v in host.items():
+                    if k.startswith("ansible_"):
+                        print(k, v)
+    except Exception as e:
+        raise AnsibleParserError('Invalid data from string, could not parse: %s' % to_native(e))
